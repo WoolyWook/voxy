917d425e687e287dd2e8f81dd946c858a06c7e00
WoolyWook <dandytony25@gmail.com>
2026-03-04 19:44:46 +0000

    Update MixinSodiumWorldRenderer.java

--- src/main/java/me/cortex/voxy/client/mixin/sodium/MixinSodiumWorldRenderer.java
+++ src/main/java/me/cortex/voxy/client/mixin/sodium/MixinSodiumWorldRenderer.java
@@ -3,66 +3,67 @@ package me.cortex.voxy.client.mixin.sodium;
 import me.cortex.voxy.client.core.IGetVoxyRenderSystem;
 import me.cortex.voxy.client.core.rendering.Viewport;
 import me.cortex.voxy.client.core.util.IrisUtil;
-import me.cortex.voxy.commonImpl.VoxyCommon;
-import me.cortex.voxy.commonImpl.VoxyInstance;
-import me.jellysquid.mods.sodium.client.gl.device.CommandList;
 import me.jellysquid.mods.sodium.client.render.SodiumWorldRenderer;
+import me.jellysquid.mods.sodium.client.render.chunk.ChunkRenderMatrices;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderType;
-import me.jellysquid.mods.sodium.client.render.chunk.ChunkRenderMatrices;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-
 @Mixin(value = SodiumWorldRenderer.class, remap = false)
 public class MixinSodiumWorldRenderer {
-    @Inject(method = "initRenderer", at = @At("TAIL"), remap = false)
-    private void voxy$injectThreadUpdate(CommandList cl, CallbackInfo ci) {
-        var vi = VoxyCommon.getInstance();
-        if (vi != null) vi.updateDedicatedThreads();
-    }
 
     @Unique
     private ChunkRenderMatrices voxy$capturedMatrices;
 
     @Inject(
-        method = "drawChunkLayer(Lnet/minecraft/client/renderer/RenderType;Lcom/mojang/blaze3d/vertex/PoseStack;DDD)V",
-        at = @At("HEAD")
+            method = "drawChunkLayer(Lnet/minecraft/class_1921;Lme/jellysquid/mods/sodium/client/render/chunk/ChunkRenderMatrices;DDD)V",
+            at = @At("HEAD"),
+            remap = false
     )
     private void voxy$captureMatrices(
             RenderType renderLayer,
-            PoseStack matrixStack,
+            ChunkRenderMatrices matrices,
             double x,
             double y,
             double z,
             CallbackInfo ci
     ) {
-        this.voxy$capturedMatrices = ChunkRenderMatrices.from(matrixStack);
+        this.voxy$capturedMatrices = matrices;
     }
 
-    @Inject(method = "drawChunkLayer(Lnet/minecraft/client/renderer/RenderType;Lcom/mojang/blaze3d/vertex/PoseStack;DDD)V", at = @At("TAIL"))
-    private void injectRender(RenderType renderLayer, PoseStack matrixStack, double x, double y, double z, CallbackInfo ci) {
+    @Inject(
+            method = "drawChunkLayer(Lnet/minecraft/class_1921;Lme/jellysquid/mods/sodium/client/render/chunk/ChunkRenderMatrices;DDD)V",
+            at = @At("TAIL"),
+            remap = false
+    )
+    private void voxy$injectRender(
+            RenderType renderLayer,
+            ChunkRenderMatrices matrices,
+            double x,
+            double y,
+            double z,
+            CallbackInfo ci
+    ) {
         this.doRender(this.voxy$capturedMatrices, renderLayer, x, y, z);
     }
-    
+
     @Unique
     private void doRender(ChunkRenderMatrices matrices, RenderType renderLayer, double x, double y, double z) {
-        if (renderLayer == RenderType.solid()) {
-            var renderer = ((IGetVoxyRenderSystem) Minecraft.getInstance().levelRenderer).getVoxyRenderSystem();
-            if (renderer != null) {
-                Viewport<?> viewport = null;
-                if (IrisUtil.irisShaderPackEnabled()) {
-                    viewport = renderer.getViewport();
-                } else {
-                    viewport = renderer.setupViewport(matrices, x, y, z);
-                }
-                renderer.renderOpaque(viewport);
-            }
+        if (renderLayer != RenderType.solid()) {
+            return;
+        }
+
+        var renderer = ((IGetVoxyRenderSystem) Minecraft.getInstance().levelRenderer).getVoxyRenderSystem();
+        if (renderer != null) {
+            Viewport<?> viewport = IrisUtil.irisShaderPackEnabled()
+                    ? renderer.getViewport()
+                    : renderer.setupViewport(matrices, x, y, z);
+
+            renderer.renderOpaque(viewport);
         }
     }
 }
